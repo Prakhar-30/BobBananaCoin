526421209ec999132d30321425f2d504cd6937ef
PRAKHAR SRIVASTAVA <130750923+Prakhar-30@users.noreply.github.com>
2024-07-15 00:22:49 +0530

    Update BobBana.sol

--- BobBana.sol
+++ BobBana.sol
@@ -1,1 +1,16 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MemeCoin is ERC20 {
+    constructor(uint256 initialSupply) ERC20("BobBanana", "BananaCoin") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
+
+//1000000000000000000000000
+
+//500000000000000000000000
